29e26f75f63a5dd15e2e4c373e959a6b822b8479
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-16 19:24:08 -0300

    Added reaction permissions

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const { DISCORD_TOKEN } = process.env;
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { DISCORD_TOKEN } = process.env
+const { Client, Collection, GatewayIntentBits } = require('discord.js')
 
 const client = new Client({
-	intents: GatewayIntentBits.Guilds,
-});
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+})
 
-client.commands = new Collection();
-client.events = new Collection();
-client.commandArray = [];
+client.commands = new Collection()
+client.events = new Collection()
+client.commandArray = []
 
-client.loadEvents = (client) => require('./handlers/events')(client);
-client.loadCommands = (client) => require('./handlers/commands')(client);
+client.loadEvents = client => require('./handlers/events')(client)
+client.loadCommands = client => require('./handlers/commands')(client)
 
-client.loadEvents(client);
-client.loadCommands(client);
+client.loadEvents(client)
+client.loadCommands(client)
 
-client.login(DISCORD_TOKEN);
+client.login(DISCORD_TOKEN)
 
-module.exports = client;
+module.exports = client
