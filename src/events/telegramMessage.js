19be7a7f7865461e5eee2067b36625404c2d9217
ArielSalgado <ariel.salgado.acevedo@gmail.com>
2022-10-16 19:23:22 -0300

    Removed useless export

--- src/events/telegramMessage.js
+++ src/events/telegramMessage.js
@@ -1,30 +1,30 @@
-require('dotenv').config();
-const Slimbot = require('slimbot');
-const { EmbedBuilder, WebhookClient } = require('discord.js');
-const { TELEGRAM_TOKEN, DISCORD_WEBHOOK } = process.env;
+require('dotenv').config()
+const Slimbot = require('slimbot')
+const { EmbedBuilder, WebhookClient } = require('discord.js')
+const { TELEGRAM_TOKEN, DISCORD_WEBHOOK } = process.env
 
-const slimbot = new Slimbot(TELEGRAM_TOKEN);
-const webhookClient = new WebhookClient({ url: DISCORD_WEBHOOK });
+const slimbot = new Slimbot(TELEGRAM_TOKEN)
+const webhookClient = new WebhookClient({ url: DISCORD_WEBHOOK })
 
-slimbot.on('message', (message) => {
-	slimbot.sendMessage(message.chat.id, 'Mensaje enviado!');
+slimbot.on('message', message => {
+  slimbot.sendMessage(message.chat.id, 'Mensaje enviado!')
 
-	const embed = new EmbedBuilder()
-		.setTitle(`${message.from.first_name} ${message.from.last_name}:`)
-		.setDescription(message.text)
-		.setColor(0x00ffff);
+  const embed = new EmbedBuilder()
+    .setTitle(`${message.from.first_name} ${message.from.last_name}:`)
+    .setDescription(message.text)
+    .setColor(0x00ffff)
 
-	webhookClient.send({
-		username: 'TelegramIntegration',
-		avatarURL: 'https://i.imgur.com/AfFp7pu.png',
-		embeds: [embed],
-	});
-});
+  webhookClient.send({
+    username: 'TelegramIntegration',
+    avatarURL: 'https://i.imgur.com/AfFp7pu.png',
+    embeds: [embed],
+  })
+})
 
-slimbot.startPolling();
+slimbot.startPolling()
 
 module.exports = {
-	slimbot,
-	name: 'telegramMessage',
-	async execute(client) {},
-};
+  slimbot,
+  name: 'telegramMessage',
+  async execute() {},
+}
